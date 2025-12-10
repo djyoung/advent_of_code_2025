d11c1b22f548a9d63218a7c6efedd31e92f8ada0
Daniel Young <me@djy.dev>
2025-12-09 20:35:15 -0800

    refactor day 1

--- src/day_1.rs
+++ src/day_1.rs
@@ -1,61 +1,81 @@
 use std::fs;
 
 const PUZZLE_INPUT_PATH: &str = "input/day_1";
+const STARTING_POSITION: usize = 50;
+const DIAL_SIZE: usize = 100;
+
+enum Direction {
+    Left,
+    Right,
+}
+
+struct Rotation {
+    direction: Direction,
+    clicks: usize,
+}
 
 pub fn run() {
     println!("day 1:");
-    println!("part 1: {}", part_1());
-    println!("part 2: {}", part_2());
-}
-
-fn part_1() -> i32 {
-    let mut position = 50;
-    let mut password = 0;
 
     let input = fs::read_to_string(PUZZLE_INPUT_PATH).unwrap();
+    let rotations = parse_rotations(&input);
 
-    for line in input.lines() {
-        let direction = &line[0..1];
-        let clicks = &line[1..].parse().unwrap();
+    println!("part 1: {}", part_1(&rotations));
+    println!("part 2: {}", part_2(&rotations));
+}
 
-        if direction == "L" {
-            position = (position - clicks + 100) % 100;
-        } else {
-            position = (position + clicks) % 100;
-        }
+fn parse_rotations(input: &str) -> Vec<Rotation> {
+    input
+        .lines()
+        .map(|line| {
+            let (direction_str, clicks_str) = line.split_at(1);
+
+            let direction = match direction_str {
+                "L" => Direction::Left,
+                _ => Direction::Right,
+            };
+
+            let clicks = clicks_str.parse().unwrap();
+
+            Rotation { direction, clicks }
+        })
+        .collect()
+}
+
+fn part_1(rotations: &[Rotation]) -> usize {
+    let mut position = STARTING_POSITION;
+    let mut count = 0;
+
+    for rotation in rotations {
+        position = match rotation.direction {
+            Direction::Left => (position + DIAL_SIZE - rotation.clicks) % DIAL_SIZE,
+            Direction::Right => (position + rotation.clicks) % DIAL_SIZE,
+        };
 
         if position == 0 {
-            password += 1;
+            count += 1;
         }
     }
 
-    password
+    count
 }
 
-fn part_2() -> i32 {
-    let mut position = 50;
-    let mut password = 0;
+fn part_2(rotations: &[Rotation]) -> usize {
+    let mut position = STARTING_POSITION;
+    let mut count = 0;
 
-    let input = fs::read_to_string(PUZZLE_INPUT_PATH).unwrap();
-
-    for line in input.lines() {
-        let direction = &line[0..1];
-        let clicks = line[1..].parse().unwrap();
-
-        for _ in 0..clicks {
-            if direction == "L" {
-                position -= 1;
-            } else {
-                position += 1;
-            }
-
-            position = (position + 100) % 100;
+    for rotation in rotations {
+        for _ in 0..rotation.clicks {
+            position = match rotation.direction {
+                Direction::Left => (position + DIAL_SIZE - 1) % DIAL_SIZE,
+                Direction::Right => (position + 1) % DIAL_SIZE,
+            };
 
             if position == 0 {
-                password += 1;
+                count += 1;
             }
         }
     }
 
-    password
+    count
 }
