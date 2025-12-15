4660f3d9577e34b427f78e1a570de3ce66678d47
Daniel Young <me@djy.dev>
2025-12-14 20:22:40 -0800

    solution for day 5 part 2

--- src/day_5.rs
+++ src/day_5.rs
@@ -1,6 +1,25 @@
-use std::fs;
+use std::{cmp, fs};
 
-type Range = (usize, usize);
+#[derive(Clone)]
+struct Range {
+    start: usize,
+    end: usize,
+}
+
+impl Range {
+    fn parse(range_str: &str) -> Self {
+        let (start_str, end_str) = range_str.split_once('-').unwrap();
+
+        Range {
+            start: start_str.parse().unwrap(),
+            end: end_str.parse().unwrap(),
+        }
+    }
+
+    fn contains(&self, value: usize) -> bool {
+        self.start <= value && value <= self.end
+    }
+}
 
 const PUZZLE_INPUT_PATH: &str = "input/day_5";
 
@@ -8,43 +27,56 @@ pub fn run() {
     println!("day 5:");
 
     let input = fs::read_to_string(PUZZLE_INPUT_PATH).unwrap();
-    let (fresh_ingredients, required_ingredients) = parse_ingredients(&input);
+    let (ranges, values) = parse_input(&input);
 
-    println!(
-        "part 1: {}",
-        part_1(&fresh_ingredients, &required_ingredients)
-    );
+    println!("part 1: {}", part_1(&ranges, &values));
+    println!("part 2: {}", part_2(&ranges));
 }
 
-fn parse_ingredients(input: &str) -> (Vec<Range>, Vec<usize>) {
-    let lines: Vec<&str> = input.lines().collect();
-    let empty_index = lines.iter().position(|&line| line.is_empty()).unwrap();
+fn parse_input(input: &str) -> (Vec<Range>, Vec<usize>) {
+    let sections: Vec<&str> = input.split("\n\n").collect();
 
-    let fresh_ingredients = lines[..empty_index]
-        .iter()
-        .map(|range_str| parse_range(range_str))
-        .collect();
+    let ranges = sections[0].lines().map(Range::parse).collect();
 
-    let required_ingredients = lines[empty_index + 1..]
-        .iter()
+    let values = sections[1]
+        .lines()
         .map(|s| s.parse().unwrap())
-        .collect();
+        .collect::<Vec<_>>();
 
-    (fresh_ingredients, required_ingredients)
+    (ranges, values)
 }
 
-fn parse_range(range_str: &str) -> Range {
-    let (start, end) = range_str.split_once('-').unwrap();
-    (start.parse().unwrap(), end.parse().unwrap())
-}
-
-fn part_1(fresh_ingredients: &[Range], required_ingredients: &[usize]) -> usize {
-    required_ingredients
+fn part_1(ranges: &[Range], values: &[usize]) -> usize {
+    values
         .iter()
-        .filter(|&&ingredient| {
-            fresh_ingredients
-                .iter()
-                .any(|&(start, end)| start <= ingredient && ingredient <= end)
-        })
+        .copied()
+        .filter(|&value| ranges.iter().any(|range| range.contains(value)))
         .count()
+}
+
+fn merge_ranges(ranges: &[Range]) -> Vec<Range> {
+    let mut sorted_ranges = ranges.to_vec();
+    sorted_ranges.sort_by_key(|r| r.start);
+
+    let mut merged: Vec<Range> = Vec::new();
+
+    for range in sorted_ranges {
+        if let Some(last) = merged.last_mut() {
+            if range.start <= last.end {
+                last.end = cmp::max(last.end, range.end);
+            } else {
+                merged.push(range);
+            }
+        } else {
+            merged.push(range);
+        }
+    }
+    merged
+}
+
+fn part_2(ranges: &[Range]) -> usize {
+    merge_ranges(ranges)
+        .iter()
+        .map(|range| range.end - range.start + 1)
+        .sum()
 }
